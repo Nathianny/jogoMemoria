da05d8bcd2a51016fcafd81078106de731e4c7b5
Nathianny Pereira <nathianny_pereira26@hotmail.com>
2023-02-08 14:04:57 -0300

    Alteraçóes

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,55 @@ var cartoes = document.querySelectorAll(".cartao");
 var existeCartaoVirado = false;
 var primeiroCartao, segundoCartao;
 
+var resultadoJogadores = [];
+
+var itensStorage = localStorage.getItem("resultado");
+
+if (itensStorage) {
+  var resultadosExistentes = JSON.parse(itensStorage);
+} else {
+  var resultadosExistentes = [];
+}
+
+console.log(resultadoJogadores);
+
 var placar = 0;
 var tempo = 0;
 
 var intervalo = setInterval(function () {
   if (tempo == 15) {
     clearInterval(intervalo);
-    cartoes.forEach(function (cartao) {
-      cartao.addEventListener("click", virarCartao);
+    alert("Tempo esgotado!");
+    var nome = prompt("Digite o seu nome:");
+    
+    var resultado = {
+      nomeJogador: nome,
+      pontuacao: placar,
+    };
+
+    resultadosExistentes.push(resultado);
+    localStorage.setItem("resultado", JSON.stringify(resultadosExistentes));
+
+    document.getElementById("recordes").style.display = "block";
+
+    var tabela = document.getElementById("resultados");
+
+    resultadosExistentes.sort().forEach(function (resultado) {
+      var tr = document.createElement("tr");
+      var tdNome = document.createElement("td");
+      tdNome.innerHTML = resultado.nomeJogador;
+      var tdPontuacao = document.createElement("td");
+      tdPontuacao.innerHTML = resultado.pontuacao;
+      tr.appendChild(tdNome);
+      tr.appendChild(tdPontuacao);
+      tabela.appendChild(tr);
+    });
+
+      cartoes.forEach(function (cartao) {
+       cartao.removeEventListener("click", virarCartao);
     });
     return;
-}
+  }
    tempo = tempo + 1;
    document.getElementById("tempo").innerHTML = tempo;
 }, 1000);
@@ -33,8 +71,14 @@ function virarCartao() {
         segundoCartao.removeEventListener("click", virarCartao);
         placar = placar + 1;
         document.getElementById('resultado').innerHTML = placar
-      } else {
+      } else {       
+       cartoes.forEach(function (cartao) {
+        cartao.removeEventListener("click", virarCartao);
+       });
         setTimeout(function () {
+          cartoes.forEach(function (cartao) {
+            cartao.addEventListener("click", virarCartao);
+          });
           primeiroCartao.classList.remove("virado");
           segundoCartao.classList.remove("virado");
         }, 2000);
